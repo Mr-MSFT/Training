29eeb093bb02cd25dfe9468727093b5ef9aa5b24
Isiah Hudson <114113220+Mr-MSFT@users.noreply.github.com>
2026-05-19 19:37:13 -0500

    Add files via upload

--- BankApp-Deploy.ps1
+++ BankApp-Deploy.ps1
@@ -1478,9 +1478,6 @@ BEGIN
 END
 ELSE
     PRINT 'Database already exists - skipping.';
-
-ALTER DATABASE [$SqlDatabase] SET COMPATIBILITY_LEVEL = 160;
-PRINT 'Compatibility level set to 160 (SQL Server 2022).';
 "@
     try {
         if ($SqlUser -and $SqlPassword) {
